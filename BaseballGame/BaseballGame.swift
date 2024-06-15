51e0979849aac59fc1437c331feccb8930e0f66c
daesung <eogud8203@naver.com>
2024-06-15 21:55:49 +0900

    class moves, deduplication

--- BaseballGame/BaseballGame.swift
+++ BaseballGame/BaseballGame.swift
@@ -54,29 +54,80 @@ class Explain{
     }
 }
 
-/*
-class Explain{
-    func Calculation(secret: [Int], player: [Int]) -> (strikes: Int, balls: Int){
-        var strikes = 0
-        var balls = 0
+class PlayBall {
+    func startGame() {
+        let target = Baseballstart()
+        let SNum = target.start()
+        print("야구게임 시작! 세 개의 랜덤 숫자가 생성되었습니다.")
         
-        for(key, value) in player.enumerated() {
-            if secret.contains(value){
-                if secret[key] == value{
-                    strikes += 1
-                }else{
-                    balls += 1
+        let userinput = Input()
+        
+        var life = 9
+        var gameover = false
+        
+        repeat {
+            if let PNum = userinput.userInput() {
+                print("플레이어 값 입력 : \(PNum)")
+                
+                let game = Explain()
+                
+                if PNum.count == 3 {
+                    // 첫 번째 자릿수 0 예외처리
+                    if PNum[0] == 0 {
+                        print("첫째 자릿수에는 '0'이 입력될 수 없습니다.")
+                        continue
+                    }
+                                    
+                    // 중복 체크
+                    let PNumSet = Set(PNum)
+                    if PNumSet.count != 3 {
+                        print("잘못된 입력 값 입니다. 세 개의 숫자를 중복 없이 입력해주세요")
+                        continue
+                    }
+                    
+                    let result = game.Calculation(secret: SNum, player: PNum)
+                    
+                    // 스트라이크, 볼 출력
+                    if result.strikes == 3 {
+                        print("정답입니다!")
+                        gameover = true
+                    } else if result.strikes != 0 || result.balls != 0 {
+                        print("\(result.strikes) 스트라이크 ,\(result.balls) 볼")
+                    } else {
+                        print("Nothing")
+                    }
+                    
+                    // PNum.count == 3 을 통과했을 때마다 라이프 1개씩 차감.
+                    if life != 1 {
+                        print("기회가 한 번 소진되었습니다.")
+                    } else {
+                        print("기회 모두 소진")
+                        gameover = true
+                    }
+                    life -= 1
                 }
+            } 
+            else {
+                print("잘못된 입력 값 입니다. 세 개의 숫자를 입력해주세요")
             }
-        }
-        
-        if strikes == 3 {
-            print("정답입니다!")
-        } else{
-            print("실패..!")
-        }
-        
-        return (strikes,balls)
+        } while !gameover
     }
 }
-*/
+
+// chapter 1
+// 플레이어 정답 입력을 받았을 때 옵셔널로 받았기 때문에, 옵셔널 바인딩으로 값을 추출해줘야함.
+// 옵셔널 바인딩? -> if let / else
+// * 1차 시도 : result에 if let을 씌웠는데, 그러면 안 됐다.
+// 받아온 값에 if let을 사용해야함! (강조)
+// clear
+
+// chapter 2
+// 게임을 클리어 하거나 9번의 기회 소진시 게임종료
+// repact / while 문으로 clear
+
+// chapter 3
+//- ex) 정답 : 456 인 경우
+//- 435를 입력한 경우 → 1스트라이크 1볼
+//- 357를 입력한 경우 → 1스트라이크
+//- 678를 입력한 경우 → 1볼
+//- 123를 입력한 경우 → Nothing
---
+++ BaseballGame/view.swift
@@ -0,0 +1,12 @@
+//
+//  view.swift
+//  BaseballGame
+//
+//  Created by 머성이 on 6/15/24.
+//
+
+import Foundation
+
+class view{
+    
+}
